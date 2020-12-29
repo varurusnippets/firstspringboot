95440a1a58174e2a1ce4103b1a45e14eca232822
Vishal Kumar Aruru <varuru@ivycomptech.com>
2020-12-29 09:30:16 +0530

    draft

---
+++ src/main/java/com/test/firstspringboot/FirstspringbootApplication.java
@@ -0,0 +1,17 @@
+package com.test.firstspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+//@EnableEurekaClient
+@EnableSwagger2
+public class FirstspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstspringbootApplication.class, args);
+	}
+
+}
